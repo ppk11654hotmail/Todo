3873013b7ad877872f3d82ce00b1d1f710c48c2f
ppk11654hotmail <ppk11654@hotmail.com>
2015-08-17 15:20:53 +0800

    連結API網址區分

--- Todo.App/Scripts/config.js
+++ Todo.App/Scripts/config.js
@@ -3,9 +3,11 @@
 
     //jackytodo Site
     var serviceUrl = "http://jackytodo.azurewebsites.net/api/";
+
     //jackytodoPro Site
     //var serviceUrl = "http://jackytodoPro.azurewebsites.net/api/";
 
+    //本機開發 Site
     //var serviceUrl = "http://localhost:51286/api/";
 
     app.config = {
--- TodoPro.App/Scripts/config.js
+++ TodoPro.App/Scripts/config.js
@@ -1,8 +1,13 @@
 (function (global) {
     var app = global.app = global.app || {};
 
-    var serviceUrl = "http://jackytodoPro.azurewebsites.net/api/";
+    //jackytodo Site
+    var serviceUrl = "http://jackytodo.azurewebsites.net/api/";
 
+    //jackytodoPro Site
+    //var serviceUrl = "http://jackytodoPro.azurewebsites.net/api/";
+
+    //本機開發 Site
     //var serviceUrl = "http://localhost:51286/api/";
 
     app.config = {

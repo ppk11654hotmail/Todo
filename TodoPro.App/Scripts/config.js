38a55d06d1fec1b26b94f772237257ecef331522
ppk11654hotmail <ppk11654@hotmail.com>
2015-08-27 11:42:29 +0800

    20150827:修正Todo及TodoPro不同連結

--- TodoPro.App/Scripts/config.js
+++ TodoPro.App/Scripts/config.js
@@ -2,10 +2,10 @@
     var app = global.app = global.app || {};
 
     //jackytodo Site
-    var serviceUrl = "http://jackytodo.azurewebsites.net/api/";
+    //var serviceUrl = "http://jackytodo.azurewebsites.net/api/";
 
     //jackytodoPro Site
-    //var serviceUrl = "http://jackytodoPro.azurewebsites.net/api/";
+    var serviceUrl = "http://jackytodoPro.azurewebsites.net/api/";
 
     //本機開發 Site
     //var serviceUrl = "http://localhost:51286/api/";

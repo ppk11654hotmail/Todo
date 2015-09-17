8ed8724409fc9718954523d46b524fbfb70d0f8f
ppk11654hotmail <ppk11654@hotmail.com>
2015-09-17 18:20:54 +0800

    20150917 : web.api改為本機測試

--- TodoPro.App/Scripts/config.js
+++ TodoPro.App/Scripts/config.js
@@ -1,14 +1,14 @@
 (function (global) {
     var app = global.app = global.app || {};
 
-    //jackytodo Site
+    //Azure : jackytodo Site
     //var serviceUrl = "http://jackytodo.azurewebsites.net/api/";
 
-    //jackytodoPro Site
-    var serviceUrl = "http://jackytodoPro.azurewebsites.net/api/";
+    //Azure : jackytodoPro Site
+    //var serviceUrl = "http://jackytodoPro.azurewebsites.net/api/";
 
     //本機開發 Site
-    //var serviceUrl = "http://localhost:51286/api/";
+    var serviceUrl = "http://localhost:51286/api/";
 
     app.config = {
         todoItemsUrl: serviceUrl + "TodoItems/"
